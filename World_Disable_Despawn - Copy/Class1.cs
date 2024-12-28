473eeeb322bea42e23958f26822c4c8530c65933
Feldwor <BoQsc@users.noreply.github.com>
2024-12-29 00:11:11 +0200

    update

---
+++ World_Disable_Despawn - Copy/Class1.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+public class DisableZombieDespawnModlet : IModApi
+{
+    public void InitMod(Mod modInstance)
+    {
+        var harmony = new Harmony("com.example.disablezombiedespawn");
+        harmony.PatchAll();
+        Debug.Log("Disable Zombie Despawn Modlet initialized");
+    }
+}
+
+[HarmonyPatch(typeof(EntityEnemy))]
+[HarmonyPatch("canDespawn")]
+public class DisableZombieDespawnPatch
+{
+    public static bool Prefix(ref bool __result)
+    {
+        __result = false; // Prevent despawning
+        return false; // Skip original method execution
+    }
+}
